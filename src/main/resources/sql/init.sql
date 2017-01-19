b32cb2f308108c0e707b86e5ed7083dad2d5b478
Kincses Dani <danikincs@gmail.com>
2017-01-19 10:39:06 +0100

    sql update

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -1,5 +1,7 @@
-DROP TABLE IF EXISTS details;
+DROP TABLE IF EXISTS webshopuser;
 DROP TABLE IF EXISTS client;
+DROP TABLE IF EXISTS product;
+
 
 CREATE TABLE client
 (
@@ -7,6 +9,21 @@ CREATE TABLE client
   apikey VARCHAR(40) UNIQUE
 );
 
+CREATE TABLE webshopuser
+(
+  id SERIAL PRIMARY KEY NOT NULL,
+  username VARCHAR(255)
+);
+
+CREATE TABLE product
+(
+  name VARCHAR(255),
+  category VARCHAR(255),
+  default_price VARCHAR(255),
+  quantity INTEGER,
+  user_id INT REFERENCES webshopuser(id)
+
+);
 
 
 
